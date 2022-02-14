9390b050bb859bb01513efdcd26e09e9e8d394d7
Vertti <56016617+helvertti@users.noreply.github.com>
2022-02-14 21:24:10 +0200

    Uploaded to see
    Final upload of version build within Gamejam 22 time limit, which was 48 hours.

---
+++ Scripts/Controllers/SceneControllerScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /*
+    * @Author Vertti Ahlstén
+    */
+
+public class SceneControllerScript : MonoBehaviour
+{
+    private GameController gameController;
+    private NPCCharacter current;
+    private bool finished;
+    private float time = 5f, timer = 0f;
+
+    void Start()
+    {
+        gameController = GameController.Instance;
+        if (gameController.getFirstTime())
+        {
+            gameController.generateClient();
+        }
+        finished = false;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (!gameController.isCurrentNpcNull() || current != null)
+        {
+            current = gameController.getCurrentNPC();
+            GameObject[] sprites = current.getSprites();
+            sprites[0].SetActive(true);
+            sprites[1].SetActive(false);
+            sprites[2].SetActive(false);
+        }
+
+        if (finished && timer >= time)
+        {
+            current = gameController.getCurrentNPC();
+            GameObject[] sprites = current.getSprites();
+            sprites[0].SetActive(false);
+            sprites[1].SetActive(false);
+            sprites[2].SetActive(false);
+
+            gameController.setCurrentNull();
+            finished = false;
+            timer = 0f;
+        }else if (finished) {
+            current = gameController.getCurrentNPC();
+            GameObject[] sprites = current.getSprites();
+
+            sprites[0].SetActive(false);
+
+            if (current.getResult() == "good") sprites[1].SetActive(true);
+            else sprites[2].SetActive(true);
+
+            timer += 0.1f;
+        } else if (!(current.getResult() == null || current.getResult() == "") && !finished ) {
+            current = gameController.getCurrentNPC();
+            GameObject[] sprites = current.getSprites();
+            sprites[0].SetActive(false);
+            finished = true;
+        }
+        
+        if (gameController.getCurrentNPC() == null)
+        {
+            if (gameController.gameOngoing()) {
+                gameController.generateClient();
+            } else {
+                LoaderScript.Load(LoaderScript.Scene.EndingScene);
+            }
+        }
+    }
+}
---
+++ Scripts/RecipeBook/Beverage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /*
+    * @Author Vertti Ahlstén
+    */
+
+public class Beverage
+{
+    public string name;
+    public List<Ingredient> properties;
+    public bool onLocked;
+
+    public Beverage(string name, List<Ingredient> properties)
+    {
+        this.name = name;
+        this.properties = properties;
+    }
+
+    public void setOnLocked(bool locked) {
+        this.onLocked = locked;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public void addIngredient(Ingredient ing) {
+        properties.Add(ing);
+    }
+
+    public List<Ingredient> getProperties()
+    {
+        return properties;
+    }
+
+    public bool hasIngredient(string ing) {
+        for (int i = 0; i < properties.Count; i++) {
+            if (properties[i].getName() == ing) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
